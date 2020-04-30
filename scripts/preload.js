af6b69632a94538d7a8b1fce093c89e84ef51574
Tzahi Levi <thetzahilevi@gmail.com>
2020-04-30 18:03:30 +0300

    build board

--- scripts/preload.js
+++ scripts/preload.js
@@ -1,12 +1,12 @@
 // All of the Node.js APIs are available in the preload process.
 // It has the same sandbox as a Chrome extension.
 window.addEventListener('DOMContentLoaded', () => {
-    const replaceText = (selector, text) => {
-        const element = document.getElementById(selector);
-        if (element) element.innerText = text
-    };
-
-    for (const type of ['chrome', 'node', 'electron']) {
-        replaceText(`${type}-version`, process.versions[type])
-    }
+    const path = require('path');
+    const head = document.getElementsByTagName('head')[0];
+    const link  = document.createElement('link');
+    link.rel  = 'stylesheet';
+    link.type = 'text/css';
+    link.href = path.join(__dirname, 'styles', 'style.css');
+    link.media = 'all';
+    head.appendChild(link);
 });
