48c94d5f751d0eedf2b02e0965e227d306ee98ba
Tzahi Levi <thetzahilevi@gmail.com>
2020-04-30 18:24:25 +0300

    disable resizable

--- main.js
+++ main.js
@@ -5,8 +5,9 @@ const path = require('path');
 function createWindow () {
     // Create the browser window.
     const mainWindow = new BrowserWindow({
-        width: 800,
+        width: 500,
         height: 600,
+        resizable: false,
         webPreferences: {
             preload: path.join(__dirname, 'scripts', 'preload.js')
         }
